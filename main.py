242443b3388fae50d6bc96bf0757eb9c1be9e0c2
Pablodc98 <caballerodc98@gmail.com>
2026-09-10 16:05:08 -0500

    Añado comentarios en ingles al codigo, sostuve un MVP

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# This code sums two numbers 
+
+# Here we ask the user two number and convert them to float type to allow decimal inputs
 number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 
+
+# Then we print the sum of two numbers
 print(number_1 + number_2)
